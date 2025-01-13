f82b5131cef52741bdd6037839d6e9e753169060
ulankachingun <145523623+ulankachingun@users.noreply.github.com>
2025-01-13 20:54:58 +0000

    pics added

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const yesButton = document.getElementById('yesButton');
 const noButton = document.getElementById('noButton');
 const finalContent = document.getElementById('finalContent');
 
-const images = ['/pics/Thinking cat.jpg', '/pics/Asking cat.png']; // Replace with your actual image paths
+const images = ['/pics/Thinking cat.jpg', '/pics/Asking cat.png'];
 let imageIndex = 0;
 
 submitName.addEventListener('click', () => {
@@ -29,7 +29,7 @@ submitName.addEventListener('click', () => {
 
 yesButton.addEventListener('click', () => {
     buttonsContainer.style.display = 'none';
-    currentImage.src = 'pics/Happy cat.gif'; // Replace with your GIF path
+    currentImage.src = '/pics/Happy cat.gif';
     setTimeout(() => {
         imageContainer.style.display = 'none';
         finalContent.style.display = 'block';
