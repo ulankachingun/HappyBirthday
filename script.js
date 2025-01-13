1bd503ad941b66bf26932f5aca7c88abb7d158d9
ulankachingun <145523623+ulankachingun@users.noreply.github.com>
2025-01-14 05:07:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const yesButton = document.getElementById('yesButton');
 const noButton = document.getElementById('noButton');
 const finalContent = document.getElementById('finalContent');
 
-const images = ['pics/Thinking.jpg', './pics/Asking.PNG'];
+const images = ['../pics/Thinking.jpg', './pics/Asking.PNG'];
 let imageIndex = 0;
 
 submitName.addEventListener('click', () => {
